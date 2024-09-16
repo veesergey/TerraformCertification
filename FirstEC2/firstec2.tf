c0774e7e800fa85b927c99111d6fd789c09ee54a
Vitaliy Sergeychik <veesergey@gmail.com>
2024-09-15 21:50:57 -0400

    Updating ec2 folder to actually deploy an ec2

---
+++ FirstEC2/firstec2.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }   
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  //access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  //secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
+}
